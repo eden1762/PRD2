d5fd6a27d22a9cffbdd604f352f86b9058902ff3
蔡易安 Tsai Yi An <edenherorockybatman@gmail.com>
2025-05-25 21:14:17 +0800

    Create fire.py firebase CRUD

---
+++ fire.py
@@ -0,0 +1,21 @@
+import firebase_admin
+from firebase_admin import credentials, firestore
+
+cred = credentials.ApplicationDefault()
+firebase_admin.initialize_app(cred)
+db = firestore.client()
+
+def save_user(user_data):
+    doc_ref = db.collection("users").document(user_data["line_id"])
+    doc_ref.set(user_data)
+
+def get_user_by_line_id(line_id):
+    doc = db.collection("users").document(line_id).get()
+    return doc.to_dict() if doc.exists else None
+
+def save_message(line_id, text, reply):
+    db.collection("messages").add({
+        "line_id": line_id,
+        "text": text,
+        "reply": reply
+    })
